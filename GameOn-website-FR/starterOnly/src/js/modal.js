c9c34a33c7e7f0bf091b6d30be3c26c2fd6a6ff4
Kenza Filali <filali1991.univ@gmail.com>
2022-03-24 15:52:37 +0100

    update valid modal

--- GameOn-website-FR/starterOnly/src/js/modal.js
+++ GameOn-website-FR/starterOnly/src/js/modal.js
@@ -1,3 +1,5 @@
+
+const icon = document.querySelector('.icon'); /*selectionne le bouton nav*/
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -6,6 +8,7 @@ function editNav() {
     x.className = "topnav";
   }
 }
+icon.addEventListener("click", editNav);
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -13,7 +16,7 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeSpan = document.querySelector(".close"); //span close
 const modal = document.querySelector(".content"); //le modal
-const modalBody = document.querySelector(".modal-body");
+let modalBody = document.querySelector(".modal-body");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -24,22 +27,19 @@ function launchModal() {
 
 }
 /* close modal form*/
-closeSpan.addEventListener("click", closeModal);
 
-function closeModal() {
+
+const closeModal = () => {
   modalbg.style.display = "none"; //masquer le modal
 }
+closeSpan.addEventListener("click", closeModal);
 
 /*modal de confirmation*/
 
-function changeModalContent() {
+/*const changeModalContent = () => {
   //modalbg.style.display = "block";
-  closeSpan.addEventListener("click", closeModal);
   modalBody.classList.add("answer-modal");
-  modalBody.innerHTML = '<div><h2>Merci ! Votre réservation a été reçue </h2></div><div><input id="fermer" class="btn-submit button" type="submit"  value="fermer" onclick="closeModal()"/></div>';
+  //alert("i work");
+  modalBody.innerHTML = ' <form name="answer" id="answer" action="./index.html" method="get"><div><h2>Merci ! Votre réservation a été reçue </h2></div><div><input id="fermer" class="btn-submit button" type="submit"  value="fermer" onclick="closeModal()"/></div></form>';
 }
-
-
-
-
-
+*/
